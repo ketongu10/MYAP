edabe74849969de76208525de34d2078bfaefdc2
ketongu10 <ketongu10@gmail.com>
2024-04-08 00:37:31 +0300

    Added 2d angle and reflect

--- calc/func_2d.py
+++ calc/func_2d.py
@@ -1,7 +1,6 @@
-import cupy as cp
+
 import numpy as np
 from time import time
-from numba import njit
 
 
 def maxwell(x, T):
@@ -9,15 +8,32 @@ def maxwell(x, T):
     return np.exp(-x*x/2/T)/norm
 
 
-@njit(parallel=True, cache=True)
+
 def solve_angle_np(F: np.ndarray, Vx, Vy, dx, dy, dt):
     vx_pos = int(len(Vx) / 2)
     vy_pos = int(len(Vy) / 2)
-    for vx in range(vx_pos, len(Vx)-1):
-        for vy in range(vy_pos, len(Vy) - 1):
+
+    # ++
+    for vx in range(vx_pos, len(Vx)):
+        for vy in range(vy_pos, len(Vy)):
             F[:,:,vx, vy] = F[:,:,vx, vy] * (1 - Vx[vx] / dx * dt - Vy[vy] / dy * dt) + \
                             np.roll(F[:,:,vx, vy], 1, 0) * (Vx[vx] / dx * dt) + \
                             np.roll(F[:,:,vx, vy], 1, 1) * (Vy[vy] / dy * dt)
+
+
+    # +-
+    for vx in range(vx_pos, len(Vx)):
+        for vy in range(vy_pos):
+            F[:, :, vx, vy] = F[:, :, vx, vy] * (1 - Vx[vx] / dx * dt + Vy[vy] / dy * dt) + \
+                              np.roll(F[:, :, vx, vy], 1, 0) * (Vx[vx] / dx * dt) - \
+                              np.roll(F[:, :, vx, vy], -1, 1) * (Vy[vy] / dy * dt)
+    # -+
+    for vx in range(vx_pos):
+        for vy in range(vy_pos, len(Vy)):
+            F[:, :, vx, vy] = F[:, :, vx, vy] * (1 + Vx[vx] / dx * dt - Vy[vy] / dy * dt) - \
+                              np.roll(F[:, :, vx, vy], -1, 0) * (Vx[vx] / dx * dt) + \
+                              np.roll(F[:, :, vx, vy], 1, 1) * (Vy[vy] / dy * dt)
+    # --
     for vx in range(vx_pos):
         for vy in range(vy_pos):
             F[:,:,vx, vy] = F[:,:,vx, vy] * (1 + Vx[vx] / dx * dt + Vy[vy] / dy * dt) - \
@@ -26,20 +42,43 @@ def solve_angle_np(F: np.ndarray, Vx, Vy, dx, dy, dt):
 
     return F
 
+def reflect(left, right, up, down, F, N_x, N_y, Vx, Vy):
+    n_vx = len(Vx)
+    n_vy = len(Vy)
+    vx_pos = int(n_vx / 2)
+    vy_pos = int(n_vy / 2)
 
-def reflect_right(f_x: np.ndarray, v: np.array, T):
-    #right side
-    v_pos = int(len(v)/2)
-    exp_dist = maxwell(v[:v_pos], T)
-    h_t = v[v_pos:].dot(f_x[v_pos:, -1]) / np.abs(v[:v_pos]).dot(exp_dist)
-    f_x[:v_pos, -1] = h_t * exp_dist
-    return f_x
 
-def reflect_left(f_x: np.ndarray, v: np.array, T):
-    #left side
-    v_pos = int(len(v) / 2)
-    exp_dist = maxwell(v[v_pos:], T)
-    h_t = np.abs(v[:v_pos]).dot(f_x[:v_pos, 0]) / (v[v_pos:].dot(exp_dist))
-    f_x[v_pos:, 0] = h_t * exp_dist
-    #print("SUM_CHECK ", np.sum(f_x[v_pos:, -1]), np.sum(f_x[:v_pos, -1]))
-    return f_x
+
+    # up
+    for x in range(N_x):
+        exp_dist = maxwell(Vy[:vy_pos], up[x])
+        exp_f = np.abs(Vy[:vy_pos]).dot(exp_dist)
+        h_t = np.tensordot(Vy[vy_pos:], (F[x, -1, :, vy_pos:]), ([0], [1])) / exp_f
+        F[x, -1, :, :vy_pos] = np.kron(h_t, exp_dist).reshape(n_vx, vy_pos)
+
+    # down
+    for x in range(N_x):
+        exp_dist = maxwell(Vy[vy_pos:], down[x])
+        exp_f = np.abs(Vy[vy_pos:]).dot(exp_dist)
+        h_t = np.tensordot(np.abs(Vy[:vy_pos]), (F[x,0,:,:vy_pos]), ([0], [1])) / exp_f
+        F[x, -1, :, vy_pos:] = np.kron(h_t, exp_dist).reshape(n_vx, vy_pos)
+
+
+    # right
+    for y in range(N_y):
+        exp_dist = maxwell(Vx[:vx_pos], right[y])
+        exp_f = np.abs(Vx[:vx_pos]).dot(exp_dist)
+        h_t = np.tensordot(Vx[vx_pos:], (F[-1, y, vx_pos:, :]), ([0], [0])) / exp_f
+        F[-1, y, vx_pos:, :] = np.kron(h_t, exp_dist).reshape(vx_pos, n_vy)
+
+    # left
+    for y in range(N_y):
+        exp_dist = maxwell(Vx[vx_pos:], left[y])
+        exp_f = np.abs(Vx[vx_pos:]).dot(exp_dist)
+        h_t = np.tensordot(np.abs(Vx[:vx_pos]), (F[0, y, :vx_pos, :]), ([0], [0])) / exp_f
+        F[0, y, :vx_pos, :] = np.kron(h_t, exp_dist).reshape(vx_pos, n_vy)
+
+    return F
+
+
--- calc/main.py
+++ calc/main.py
@@ -1,7 +1,7 @@
-import cupy as cp
+
 import numpy as np
 from time import time
-from func_1d import *
+from func_2d import *
 
 def set_1(f_x: np.array):
     n = len(f_x)
@@ -9,33 +9,65 @@ def set_1(f_x: np.array):
     f_x[n//2-dn:n//2+dn] = np.array([1 for i in range(2*dn)])
     f_x/=f_x.sum()
 
-SIM_TIME = 10
+SIM_TIME = 25
 
-N_x = 20
+N_x ,  N_y = 20, 20
 N_v = 2
 
 T1 = 1
 T2 = 1
 
-dx = 1
+dx = dy = 1
 dze_1 = np.sqrt(T2)
 dze_cut = 4.8 * dze_1
-dt = dx / dze_cut
+dt = dx / dze_cut/2
 
 V = np.linspace(-dze_cut, dze_cut, N_v)
 assert 0 not in V
 print(V, V[1]*dt/dx)
-F_x = np.zeros((N_v, N_x), dtype=np.float32)
+F = np.zeros((N_x, N_y, N_v, N_v), dtype=np.float32)
+F[0,0,1,1] = 1 #pointed gas moves up and right
+F/=F.sum()
 
-for v, velocity in enumerate(V):
-    set_1(F_x[v])
-F_x/=F_x.sum()
+left = right = up = down = np.array([T1 for i in range(N_x)], dtype=np.float32)
+def start_2d(F, V, dx, dy, dt):
+    START_TIME = time()
+    for t in range(SIM_TIME):
+        F = solve_angle_np(F, V, V, dx, dy, dt)
+        t1 = time()
+        F = reflect(left, right, up, down, F, N_x, N_y, V ,V)
+        print(np.where(F > 0.2), F[1,1,1,1])
+    print(f"TIME = {time() - START_TIME} s, {time() - t1}")
 
-START_TIME = time()
-for t in range(SIM_TIME):
-    for v, velocity in enumerate(V):
-        F_x[v] = solve_angle_np(F_x[v], velocity, dx, dt)
-    reflect_right(F_x, V, T2)
-    reflect_left(F_x, V, T1)
-print(f"END_TIME = {time()-START_TIME} s")
+
+start_2d(F, V, dx, dy, dt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def start_1d():
+    START_TIME = time()
+    for t in range(SIM_TIME):
+        for v, velocity in enumerate(V):
+            F_x[v] = solve_angle_np(F_x[v], velocity, dx, dt)
+        reflect_right(F_x, V, T2)
+        reflect_left(F_x, V, T1)
+    print(f"END_TIME = {time()-START_TIME} s")
 #print(F_x, np.sum(F_x[4]))
+
